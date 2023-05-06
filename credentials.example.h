275d7cc637567375c2b816cede3a031fef071492
tsomic <keller.nils99@gmail.com>
2023-05-06 12:25:20 +0200

    add JSON parsing and distance calulation

--- credentials.example.h
+++ credentials.example.h
@@ -2,3 +2,7 @@
 
 #define WIFI_SSID "your-ssid"
 #define WIFI_PASSWORD "your-password"
+#define API_KEY "your-api-key"
+#define ARRIVAL_IATA "your-arrival-airport"
+#define ARRIVAL_LAT 0.00
+#define ARRIVAL_LNG 0.00
--- sky-schedule.ino
+++ sky-schedule.ino
@@ -1,25 +1,134 @@
+#include <ArduinoJson.h>
 #include <WiFi.h>
+#include <math.h>
+
 #include "credentials.h"
 
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PASSWORD;
+#define RADIUS_EARTH 6371.0
+
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWORD;
+
+typedef struct {
+    String reg_number;
+    String flag;
+    float lat;
+    float lng;
+    int alt;
+    int dir;
+    int speed;
+    int v_speed;
+    String flight_iata;
+    String airline_iata;
+    String aircraft_icao;
+    String status;
+} FlightData;
+
+typedef struct {
+    double distance;
+    int index;
+} ClosestFlight;
+
+FlightData *flights;
+int numFlights = 0;
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+    getFlightData();
+
+    delay(2000);
+    Serial.println(WiFi.localIP());
 }
 
+int dataIndex = 0;
+ClosestFlight closestFlight = {infinity(), -1};
+
 void loop() {
-    Serial.println(WiFi.localIP());
+    if (dataIndex < numFlights) {
+        if (flights[dataIndex].status == "en-route") {
+            double distance = printFlightDataInfo(dataIndex);
+            Serial.println(distance);
+            if (distance < closestFlight.distance) {
+                closestFlight = {distance, dataIndex};
+            }
+        }
+        dataIndex++;
+    }
+    Serial.println("Closest:");
+    Serial.println(closestFlight.distance);
+    Serial.println(closestFlight.index);
     delay(1000);
+}
+
+double printFlightDataInfo(int dataIndex) {
+    FlightData data = flights[dataIndex];
+    Serial.println(data.reg_number);
+    double distance =
+        calculateDistance(ARRIVAL_LAT, ARRIVAL_LNG, data.lat, data.lng);
+    return distance;
+}
+
+double toRadians(double degrees) { return degrees * (PI / 180.0); }
+
+double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    double dLat = toRadians(lat2 - lat1);
+    double dLon = toRadians(lon2 - lon1);
+
+    double a = sin(dLat / 2.0) * sin(dLat / 2.0) +
+               cos(toRadians(lat1)) * cos(toRadians(lat2)) * sin(dLon / 2.0) *
+                   sin(dLon / 2.0);
+
+    double c = 2.0 * atan2(sqrt(a), sqrt(1 - a));
+
+    double distance = RADIUS_EARTH * c;
+    return distance;
+}
+
+void getFlightData() {
+    // TODO make HTTP request
+    String jsonString = "{\"response\":[]}";
+
+    DynamicJsonDocument doc(4096);
+    DeserializationError error = deserializeJson(doc, jsonString);
+
+    if (error) {
+        Serial.print("JSON parsing error: ");
+        Serial.println(error.c_str());
+        return;
+    }
+
+    JsonArray flightsArray = doc["response"];
+
+    numFlights = flightsArray.size();
+    flights = new FlightData[numFlights];
+
+    int i = 0;
+    for (JsonObject flightObj : flightsArray) {
+        flights[i] = {flightObj["reg_number"].as<String>(),
+                      flightObj["flag"].as<String>(),
+                      flightObj["lat"].as<float>(),
+                      flightObj["lng"].as<float>(),
+                      flightObj["alt"].as<int>(),
+                      flightObj["dir"].as<int>(),
+                      flightObj["speed"].as<int>(),
+                      flightObj["v_speed"].as<int>(),
+                      flightObj["flight_iata"].as<String>(),
+                      flightObj["airline_iata"].as<String>(),
+                      flightObj["aircraft_icao"].as<String>(),
+                      flightObj["status"].as<String>()};
+
+        i++;
+    }
 }
